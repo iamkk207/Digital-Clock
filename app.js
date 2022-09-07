68f5b60bd14d50915d4adf8464ee2bf1c48ca5fb
iamkk207 <109844871+iamkk207@users.noreply.github.com>
2022-09-08 02:50:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 function currentTime2(){
-    let time = new Date().toLocaleTimeString('en-US');
+    let time = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', second: '2-digit', hour12: true});
     document.getElementById("digitalclock").innerHTML=time;
 }
 
